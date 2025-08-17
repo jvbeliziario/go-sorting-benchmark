5cb8b18d5e7da1bfab90a2ec85e0dd6bb771c83b
guiwoch <guilhermewochner@gmail.com>
2025-08-17 12:05:14 -0300

    Insertion Sort

---
+++ algorithms/insertion.go
@@ -0,0 +1,24 @@
+package algorithms
+
+func InsertionSort(arr []int) (stats SortingStats) {
+
+	for i := 1; i < len(arr); i++ {
+		
+		j := i
+
+		for j > 0 {
+			stats.Comparisons++
+			if arr[j-1] > arr[j] {
+
+				stats.Swaps++
+				arr[j-1], arr[j] = arr[j], arr[j-1]
+				
+				j--
+			} else {
+				break
+			}
+		}
+	}
+
+	return stats
+}
